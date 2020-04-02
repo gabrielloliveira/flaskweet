d7c1ac1e19672deb39f68e4bc8f31cbac35ab6b9
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2020-04-02 16:12:10 -0300

    adicionado templates de cadastro e login do usuário

--- core/controllers/urls.py
+++ core/controllers/urls.py
@@ -3,8 +3,14 @@ from core import app
 
 
 @app.route('/')
+@app.route('/login/')
 def index():
-    return home()
+    return render_template('login.html')
+
+
+@app.route('/cadastro/')
+def signup():
+    return render_template('signup.html')
 
 
 @app.route('/home/')
