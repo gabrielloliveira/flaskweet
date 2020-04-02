47b3342c9cc94c7dbe17f05fd7562acaf2c23420
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2020-04-02 13:15:25 -0300

    adicionado arquivo da representação do banco de dados

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-
 app.config.update(
     ENV="development",
     DEBUG=True,
 )
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
 
 from .controllers import urls
---
+++ core/models/tables.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from core import db
+
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(), unique=True, nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(), nullable=True)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+
+class Tweet(db.Model):
+    __tablename__ = "tweet"
+
+    id = db.Column(db.Integer, primary_key=True)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', foreign_keys=user_id)
+
+    def __init__(self, content, user_id):
+        self.content = content
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f'<Tweet: {self.id}>'
+
+
+class Follow(db.Model):
+    __tablename__ = "follow"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    follower_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', foreign_keys=user_id)
+    follower = db.relationship('User', foreign_keys=follower_id)
