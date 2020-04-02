2ee97e543a7300b0ca21d1bb110fecb14415c3de
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2020-04-02 15:46:51 -0300

    renderizando template base

--- core/controllers/urls.py
+++ core/controllers/urls.py
@@ -1,6 +1,12 @@
+from flask import render_template
 from core import app
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return home()
+
+
+@app.route('/home/')
+def home():
+    return render_template('home.html')
